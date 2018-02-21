cca784f6a759789c485f15708d7db6929f348ac5
Student <iliana.katinakis@mail.mcgill.ca>
2018-02-21 18:58:01 -0500

    SpringBootApp now works

--- TreePLE-Spring/src/main/java/ca/mcgill/ecse321/treePLE/TreePLESpringApplication.java
+++ TreePLE-Spring/src/main/java/ca/mcgill/ecse321/treePLE/TreePLESpringApplication.java
@@ -1,22 +1,31 @@
 package ca.mcgill.ecse321.treePLE;
 
-import ca.mcgill.ecse321.treePLE.model.TreePLEManager;
-import ca.mcgill.ecse321.treePLE.persistence.PersistenceXStream;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.config.Configuration.AccessLevel;
 import org.modelmapper.convention.NamingConventions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-import ca.mcgill.ecse321.treePLE.TreePLESpringApplication;
 import ca.mcgill.ecse321.treePLE.controller.configuration.AndroidProperties;
 import ca.mcgill.ecse321.treePLE.controller.configuration.WebFrontendProperties;
+import ca.mcgill.ecse321.treePLE.model.TreePLEManager;
+import ca.mcgill.ecse321.treePLE.persistence.PersistenceXStream;
 
-public class TreePLESpringApplication {
+@SpringBootApplication
+public class TreePLESpringApplication extends SpringBootServletInitializer {	
+
+	@Autowired
+	private AndroidProperties androidProperties;
+
+	@Autowired
+	private WebFrontendProperties webFrontendProperties;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TreePLESpringApplication.class, args);
 	}
@@ -31,33 +40,25 @@ public class TreePLESpringApplication {
 		return modelMapper;
 	}
 	
-	
-	// TODO add a Bean to provide a registration manager
-		@Bean
-		public TreePLEManager treeMan() {
-			return PersistenceXStream.initializeModelManager(PersistenceXStream.getFilename());
-		}
-
-		// TODO add client configuration
-		@Autowired
-		private AndroidProperties androidProperties;
-
-		@Autowired
-		private WebFrontendProperties webFrontendProperties;
+	//Bean to provide a TreePLE manager
+	@Bean
+	public TreePLEManager treeMan() {
+		return PersistenceXStream.initializeModelManager(PersistenceXStream.getFilename());
+	}
 		
-		// Enable CORS globally
-		@Bean
-		public WebMvcConfigurer corsConfigurer() {
-			return new WebMvcConfigurerAdapter() {
-				@Override
-				public void addCorsMappings(CorsRegistry registry) {
-					// Allow web client
-					String frontendUrl = "http://" + webFrontendProperties.getIp() + ":" + webFrontendProperties.getPort();
-					// Allow android client
-					String androidUrl = "http://" + androidProperties.getIp() + ":" + androidProperties.getPort();
-					// For debug purposes, allow connecting  from localhost as well
-					registry.addMapping("/**").allowedOrigins(frontendUrl, androidUrl, "http://localhost:8087", "http://127.0.0.1:8087");
-				}
-			};
-		}
+	// Enable CORS globally
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				// Allow web client
+				String frontendUrl = "http://" + webFrontendProperties.getIp() + ":" + webFrontendProperties.getPort();
+				// Allow android client
+				String androidUrl = "http://" + androidProperties.getIp() + ":" + androidProperties.getPort();
+				// For debug purposes, allow connecting  from localhost as well
+				registry.addMapping("/**").allowedOrigins(frontendUrl, androidUrl, "http://localhost:8087", "http://127.0.0.1:8087");
+			}
+		};
+	}
 }
