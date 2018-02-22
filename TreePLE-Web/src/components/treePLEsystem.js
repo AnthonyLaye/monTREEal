26cd4453ea2df6b13c67b8c19df5b68ab6ddcdbf
Student <iliana.katinakis@mail.mcgill.ca>
2018-02-21 19:45:24 -0500

    Web frontend started (template)

---
+++ TreePLE-Web/src/components/treePLEsystem.js
@@ -0,0 +1,6 @@
+//registration.js file for Assignment 1
+
+//1. Create constructor methods (Dtos)
+//2. Add data variables to the export declaration of the component
+//3. Add an initialization function below the data part
+//4. Add methods
