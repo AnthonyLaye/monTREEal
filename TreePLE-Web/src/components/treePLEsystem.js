454a6501b84179adf42271594955e301f6853d17
Student <iliana.katinakis@mail.mcgill.ca>
2018-02-22 20:22:02 -0500

    Web - dynamic binding

--- TreePLE-Web/src/components/treePLEsystem.js
+++ TreePLE-Web/src/components/treePLEsystem.js
@@ -1,6 +1,42 @@
-//registration.js file for Assignment 1
-
 //1. Create constructor methods (Dtos)
 //2. Add data variables to the export declaration of the component
 //3. Add an initialization function below the data part
 //4. Add methods
+
+import axios from 'axios'
+var config = require('../../config')
+
+var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
+var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
+
+var AXIOS = axios.create({
+  baseURL: backendUrl,
+  headers: { 'Access-Control-Allow-Origin': frontendUrl }
+})
+
+function TreeDto(species, height, age, date, diameter, id) {
+  this.species = species;
+  this.height = height;
+  this.age = age;
+  this.date = date;
+  this.diameter = diameter;
+  this.id = id;
+}
+export default {
+  name: 'treePLEsystem',
+  data() {
+    return {
+      trees: [],
+      errorTrees: ''
+    }
+  },
+  created: function () {
+    AXIOS.get('/trees')
+    .then(response => {
+      this.trees = response.data
+    })
+    .catch(e => {
+      this.errorEvent = e;
+    });
+  }
+}
