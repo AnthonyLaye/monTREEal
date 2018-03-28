aa6072733f4bd9f7c9e68f8537a1e1913536277a
WenzongXia <wenzong.xia@mail.mcgill.ca>
2018-03-28 18:40:44 -0400

    test for jenkins

--- TreePLE-Spring/src/test/java/ca/mcgill/ecse321/test/persistence/TestPersistence.java
+++ TreePLE-Spring/src/test/java/ca/mcgill/ecse321/test/persistence/TestPersistence.java
@@ -38,44 +38,44 @@ public class TestPersistence {
 		tm.delete();
 	}
 
-//	@Test
-//	public void test() {
-//		 PersistenceXStream.initializeModelManager("output"+File.separator+"data.xml");
-//		    // save model that is loaded during test setup
-//		    if (!PersistenceXStream.saveToXMLwithXStream(tm))
-//		    	fail("Could not save file.");s
-//		
-//		    tm.delete();
-//		    assertEquals(0, tm.getTrees().size());
-//		    
-//		    tm = (TreePLEManager) PersistenceXStream.loadFromXMLwithXStream();
-//		    if(tm == null)
-//		    	fail("Could not load file.");
-//		    	
-//		    //check trees
-//		    assertEquals(2, tm.getTrees().size());
-//
-//			Assert.assertEquals("oak",tm.getTree(0).getSpecies());
-//			Assert.assertEquals("maple",tm.getTree(1).getSpecies());
-//		    
-//		    Calendar c = Calendar.getInstance();
-//		    c.set(2018, 02, 04);
-//
-//			Assert.assertEquals(c,tm.getTree(0).getDate());
-//		    c.set(2018, 02, 03);
-//			Assert.assertEquals(c,tm.getTree(1).getDate());
-//
-//			Assert.assertEquals(21,tm.getTree(0).getId());
-//			Assert.assertEquals(22,tm.getTree(1).getId());
-//		    
-//		    Assert.assertEquals(new Person("Jeff",tm),tm.getTree(0).getPerson());
-//			Assert.assertEquals(new Person("George",tm),tm.getTree(1).getPerson());
-//
-//			Assert.assertEquals(tm,tm.getTree(0).getTreePLEManager());
-//			Assert.assertEquals(tm,tm.getTree(1).getTreePLEManager());
-//
-//			Assert.assertEquals(new Location(2.00f,2.00f, "NDG"),tm.getTree(0).getLocation());
-//			Assert.assertEquals(new Location(3.05f,9.00f, "Outremont"),tm.getTree(1).getLocation());
-//	}
+	@Test
+	public void test() {
+		 PersistenceXStream.initializeModelManager("output"+File.separator+"data.xml");
+		    // save model that is loaded during test setup
+		    if (!PersistenceXStream.saveToXMLwithXStream(tm))
+		    	fail("Could not save file.");
+		
+		    tm.delete();
+		    assertEquals(0, tm.getTrees().size());
+		    
+		    tm = (TreePLEManager) PersistenceXStream.loadFromXMLwithXStream();
+		    if(tm == null)
+		    	fail("Could not load file.");
+		    	
+		    //check trees
+		    assertEquals(2, tm.getTrees().size());
+
+			Assert.assertEquals("oak",tm.getTree(0).getSpecies());
+			Assert.assertEquals("maple",tm.getTree(1).getSpecies());
+		    
+		    Calendar c = Calendar.getInstance();
+		    c.set(2018, 02, 04);
+
+			Assert.assertEquals(c,tm.getTree(0).getDate());
+		    c.set(2018, 02, 03);
+			Assert.assertEquals(c,tm.getTree(1).getDate());
+
+			Assert.assertEquals(21,tm.getTree(0).getId());
+			Assert.assertEquals(22,tm.getTree(1).getId());
+		    
+		    Assert.assertEquals(new Person("Jeff",tm),tm.getTree(0).getPerson());
+			Assert.assertEquals(new Person("George",tm),tm.getTree(1).getPerson());
+
+			Assert.assertEquals(tm,tm.getTree(0).getTreePLEManager());
+			Assert.assertEquals(tm,tm.getTree(1).getTreePLEManager());
+
+			Assert.assertEquals(new Location(2.00f,2.00f, "NDG"),tm.getTree(0).getLocation());
+			Assert.assertEquals(new Location(3.05f,9.00f, "Outremont"),tm.getTree(1).getLocation());
+	}
 
 }
