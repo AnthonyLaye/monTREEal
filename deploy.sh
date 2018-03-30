c67e3dc563077a3df17eb62411caa3c9a5d1ee90
marine.huynhhuu <marine.huynhhuu@mail.mcgill.ca>
2018-03-30 17:57:36 -0400

    Not copying onto ROOT.war new war file

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cp /var/lib/jenkins/workspace/TreePLE14/TreePLE-Spring/build/libs/treePLE-0.0.1-SNAPSHOT.war /opt/tomcat/webapps/ROOT.war
+cp /var/lib/jenkins/workspace/TreePLE14/TreePLE-Spring/build/libs/treePLE-0.0.1-SNAPSHOT.war /opt/tomcat/webapps/tree.war
 systemctl start tomcat
 systemctl status tomcat
