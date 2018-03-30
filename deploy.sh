a35fb4bf65460c0a82dcb38844e5b0c1c721984c
WenzongXia <wenzong.xia@mail.mcgill.ca>
2018-03-30 12:37:29 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo"HelloWorld"
-cp /var/lib/jenkins/workspace/TreePLE14/TreePLE-Spring/build/libs/treePLE-0.0.1-SNAPSHOT.war /opt/tomcat/webapps/ROOT.war
+sudo cp /var/lib/jenkins/workspace/TreePLE14/TreePLE-Spring/build/libs/treePLE-0.0.1-SNAPSHOT.war /opt/tomcat/webapps/ROOT.war
+sudo chown tomcat -R /opt/tomcat/
 systemctl start tomcat
 systemctl status tomcat
