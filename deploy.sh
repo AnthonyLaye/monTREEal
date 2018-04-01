ea21dcd122877ecee4eef0783106ffe55d195446
marine.huynhhuu <marine.huynhhuu@mail.mcgill.ca>
2018-04-01 18:38:27 -0400

    deploy modifs

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cp /var/lib/jenkins/workspace/TreePLE14/TreePLE-Spring/build/libs/treePLE-0.0.1-SNAPSHOT.war /opt/tomcat/webapps/tree.war
-systemctl start tomcat
+cp /var/lib/jenkins/workspace/TreePLE14/TreePLE-Spring/build/libs/treePLE-0.0.1-SNAPSHOT.war /opt/tomcat/webapps/treePLE.war
+systemctl restart tomcat
 systemctl status tomcat
