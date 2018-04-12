24f029cb1f2ccfa7fbdc86454be646ef06dd432e
Anthony Laye <anthony.laye@mail.mcgill.ca>
2018-04-12 16:34:12 -0400

    Basic calculation for biodiversity index

--- TreePLE-Web/src/components/biodiversityindex.js
+++ TreePLE-Web/src/components/biodiversityindex.js
@@ -2,6 +2,10 @@ export default {
   name: 'BiodiversityIndex',
   data () {
     return {
+      biodiversityindex: 1,
+      latitude: 1,
+      longitude: 1,
+      radius: 0
     }
   },
 
@@ -17,9 +21,21 @@ export default {
     },
     calculateIndex: function(){
 
+      AXIOS.get('/treePLE/trees/forecast/biodiversity' + '?latitude=' + latitude + '&longitude=' + longitude, + '&distance=' +distance, {}, {})
+      .then(response => {
+        // JSON responses are automatically parsed.
+        this.biodiversityindex.push(response.data)
+        this.latitude = 0
+        this.longitude = 0
+        this.radius = 0
+      })
+      .catch(e => {
+        var errorMsg = e.message
+        console.log(errorMsg)
+      });
     },
     addToForecast: function(){
-      
+
     }
   }
 }
