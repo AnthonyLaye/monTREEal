27a396180c2c8d3f46091670e2786ccb40a202e3
marine.huynhhuu <marine.huynhhuu@mail.mcgill.ca>
2018-04-12 22:09:29 -0400

    changed model for Densities

--- TreePLE-Spring/src/main/java/ca/mcgill/ecse321/treePLE/model/SpeciesDensities.java
+++ TreePLE-Spring/src/main/java/ca/mcgill/ecse321/treePLE/model/SpeciesDensities.java
@@ -1,10 +1,10 @@
 /*PLEASE DO NOT EDIT THIS CODE*/
-/*This code was generated using the UMPLE 1.27.0.3860.40605acef modeling language!*/
+/*This code was generated using the UMPLE 1.27.1.3862.7e232c4b4 modeling language!*/
 
 package ca.mcgill.ecse321.treePLE.model;
 
-// line 63 "../../../../../../../../ump/180117838895/model.ump"
-// line 117 "../../../../../../../../ump/180117838895/model.ump"
+// line 63 "../../../../../../../../ump/tmp859800/model.ump"
+// line 119 "../../../../../../../../ump/tmp859800/model.ump"
 public class SpeciesDensities
 {
 
@@ -15,6 +15,7 @@ public class SpeciesDensities
   //SpeciesDensities Attributes
   private String species;
   private int density;
+  private String UISpecies;
 
   //SpeciesDensities Associations
   private CarbonSequestrationManager CarbonSequestrationManager;
@@ -23,10 +24,11 @@ public class SpeciesDensities
   // CONSTRUCTOR
   //------------------------
 
-  public SpeciesDensities(String aSpecies, int aDensity, CarbonSequestrationManager aCarbonSequestrationManager)
+  public SpeciesDensities(String aSpecies, int aDensity, String aUISpecies, CarbonSequestrationManager aCarbonSequestrationManager)
   {
     species = aSpecies;
     density = aDensity;
+    UISpecies = aUISpecies;
     boolean didAddCarbonSequestrationManager = setCarbonSequestrationManager(aCarbonSequestrationManager);
     if (!didAddCarbonSequestrationManager)
     {
@@ -54,6 +56,14 @@ public class SpeciesDensities
     return wasSet;
   }
 
+  public boolean setUISpecies(String aUISpecies)
+  {
+    boolean wasSet = false;
+    UISpecies = aUISpecies;
+    wasSet = true;
+    return wasSet;
+  }
+
   public String getSpecies()
   {
     return species;
@@ -62,6 +72,11 @@ public class SpeciesDensities
   public int getDensity()
   {
     return density;
+  }
+
+  public String getUISpecies()
+  {
+    return UISpecies;
   }
   /* Code from template association_GetOne */
   public CarbonSequestrationManager getCarbonSequestrationManager()
@@ -103,7 +118,8 @@ public class SpeciesDensities
   {
     return super.toString() + "["+
             "species" + ":" + getSpecies()+ "," +
-            "density" + ":" + getDensity()+ "]" + System.getProperties().getProperty("line.separator") +
+            "density" + ":" + getDensity()+ "," +
+            "UISpecies" + ":" + getUISpecies()+ "]" + System.getProperties().getProperty("line.separator") +
             "  " + "CarbonSequestrationManager = "+(getCarbonSequestrationManager()!=null?Integer.toHexString(System.identityHashCode(getCarbonSequestrationManager())):"null");
   }
 }
