f5ae42b2b64db1568c3fa762430063e5dff2278f
marine.huynhhuu <marine.huynhhuu@mail.mcgill.ca>
2018-04-14 18:04:43 -0400

    update path to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cp /var/lib/jenkins/workspace/TreePLE14/TreePLE-Spring/build/libs/treePLE-0.0.1-SNAPSHOT.war /opt/tomcat/webapps/treePLE.war
+cp /var/lib/jenkins/workspace/TreePLE14-Spring/TreePLE-Spring/build/libs/treePLE-0.0.1-SNAPSHOT.war /opt/tomcat/webapps/treePLE.war
 systemctl restart tomcat
 systemctl status tomcat
